8dd3cd8c3e0a1de5e2285527da2c07f48c8f7f7c
a <a>
2022-06-09 16:44:19 +0200

    after merge fix

--- acuicvjezba1/NewClass.cs
+++ acuicvjezba1/NewClass.cs
@@ -6,13 +6,8 @@ namespace acuicvjezba1
 {
     class NewClass
     {
-<<<<<<< HEAD
         public void print() {
             Console.WriteLine("Ocemo konfliiikt");
-=======
-        public void ispisi(){
-        console.writeline("ispis");
->>>>>>> be9bda08791b2d89abba855033c77105c2aae483
         }
     }
 }
